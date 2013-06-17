214236a1dc971e95550f7611f1eb4206f114e469
Viraj Parikh <virajparikh@gmail.com>
2013-06-16 19:27:20 -0500

    file modifications

---
+++ features/step_definitions/register_login_steps.rb
@@ -0,0 +1,24 @@
+When(/^I go to the homepage$/) do
+  visit "/"
+end
+
+And(/^I (?:click|press) "(.*?)"$/) do |text|
+  click_link_or_button text
+end
+
+And(/^I should not see "(.*?)"$/) do |content|
+  page.should_not have_content(content)
+end
+
+And(/^I fill in "(.*?)" for "(.*?)"$/) do |value, email|
+  fill_in email, with: value
+end
+
+Then(/^I should see "(.*?)"$/) do |content|
+  page.should have_content(content)
+end
+
+Given(/^the user "(.*?)" with password "(.*?)"$/) do |email, password|
+  user = User.create( email: email, password: password)
+  refute user.new_record?
+end
